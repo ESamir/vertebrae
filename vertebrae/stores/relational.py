021a58a2b3b86195059dd780979c302f0d3f9016
Mahina Kaholokula <mahina.kaholokula@prelude.org>
2022-09-23 07:25:56 -0700

    create db if does not exist (#6)
    * create db if does not exist
    
    * create db if does not exist
    
    * extract pool execute
    
    Co-authored-by: Francis Mireault <mireaulf@gmail.com>

--- vertebrae/stores/relational.py
+++ vertebrae/stores/relational.py
@@ -1,6 +1,6 @@
-import logging
-
 import aiopg
+import logging
+import psycopg2
 
 from vertebrae.config import Config
 
@@ -10,36 +10,53 @@ class Relational:
     def __init__(self):
         self._pool = None
 
+    @staticmethod
+    async def __pool_execute(pool, statement, params = None, cursor_lambda = None):
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(statement, params)
+                if cursor_lambda:
+                    return await cursor_lambda(cur)
+
     async def connect(self) -> None:
         """ Establish a connection to Postgres """
         postgres = Config.find('postgres')
         if postgres:
-            self._pool = await aiopg.create_pool(f"dbname={postgres['database']} "
-                                                 f"user={postgres['user']} "
-                                                 f"password={postgres['password']} "
-                                                 f"host={postgres['host']} "
-                                                 f"port={postgres['port']} ",
-                                                 minsize=0, maxsize=5, timeout=10.0)
+            dsn = (f"user={postgres['user']} "
+                   f"password={postgres['password']} "
+                   f"host={postgres['host']} "
+                   f"port={postgres['port']} ")
+            try:
+                self._pool = await aiopg.create_pool(dsn + f"dbname={postgres['database']} ",
+                                                     minsize=0, maxsize=5, timeout=10.0)
+                await self.__pool_execute(self._pool, f"SELECT FROM pg_database WHERE datname = '{postgres['database']};'")
+            except psycopg2.OperationalError:
+                logging.debug(f"Database '{postgres['database']}' does not exist")
+                async with aiopg.create_pool(dsn, minsize=0, maxsize=5, timeout=10.0) as sys_conn:
+                    await self.__pool_execute(sys_conn, f"CREATE DATABASE {postgres['database']};")
+                logging.debug(f"Created database '{postgres['database']}'")
+                self._pool = await aiopg.create_pool(dsn + f"dbname={postgres['database']} ",
+                                                     minsize=0, maxsize=5, timeout=10.0)
             with open('conf/schema.sql', 'r') as sql:
                 await self.execute(sql.read())
 
     async def execute(self, statement: str, params=(), return_id=False):
         """ Run statement retrieving either nothing or the row ID """
+        async def cursor_operation(cur):
+           if return_id:
+               return (await cur.fetchone())[0]
+
         try:
-            async with self._pool.acquire() as conn:
-                async with conn.cursor() as cur:
-                    await cur.execute(statement, params)
-                    if return_id:
-                        return (await cur.fetchone())[0]
+            return await self.__pool_execute(self._pool, statement, params, cursor_operation)
         except Exception as e:
             logging.exception(e)
 
     async def fetch(self, query: str, params=()):
         """ Find all matches for a query """
+        async def cursor_operation(cur):
+           return await cur.fetchall()
+
         try:
-            async with self._pool.acquire() as conn:
-                async with conn.cursor() as cur:
-                    await cur.execute(query, params)
-                    return await cur.fetchall()
+            return await self.__pool_execute(self._pool, query, params, cursor_operation)
         except Exception as e:
             logging.exception(e)
