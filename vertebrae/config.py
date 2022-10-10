cfdc6a697293cf8b561fb410378a686ffc4f1a16
Mahina Kaholokula <mahina.kaholokula@prelude.org>
2022-10-10 05:52:16 -0700

    lowercase env variable keys (#25)

--- vertebrae/config.py
+++ vertebrae/config.py
@@ -17,11 +17,16 @@ class Config:
     @classmethod
     def find(cls, prop=None, fallback=None):
         """ Find a property """
-        return cls._configs.get(prop, fallback)
+        return cls._configs.get(prop.lower(), fallback)
 
     @staticmethod
     def strip(env):
         """ Inject config properties from env variables """
+        def lower_keys(dct):
+            if isinstance(dct, dict):
+                return {k.lower(): lower_keys(v) for k, v in dct.items()}
+            return dct
+
         def dict_merge(dct, merge_dct):
             for k, v in merge_dct.items():
                 if k in dct and isinstance(dct[k], dict) and isinstance(merge_dct[k], dict):
@@ -35,6 +40,8 @@ class Config:
                     return next(yaml.load_all(seed, Loader=yaml.FullLoader))
             return ''
 
-        env = strip_yml(env)
-        dict_merge(env, {k: json.loads(os.getenv(k, '{}')) if isinstance(v, dict) else os.getenv(k, v) for k, v in env.items()})
+        env = lower_keys(strip_yml(env))
+        os_keys = [k for k in list(os.environ.keys()) if k.lower() in env.keys()]
+        os_env = {k: json.loads(os.getenv(k)) if isinstance(env[k.lower()], dict) else os.getenv(k) for k in os_keys}
+        dict_merge(env, lower_keys(os_env))
         return env
